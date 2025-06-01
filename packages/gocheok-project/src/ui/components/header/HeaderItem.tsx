a1e27da5217d932f5ac0c070e190515f859320a8
MinByeongChan <mbc0481@naver.com>
2025-06-01 16:33:11 +0900

    feat(gocheok-project): hover 수정 - css 파일 제거, tailwind 문법으로 변경

--- packages/gocheok-project/src/ui/components/header/HeaderItem.tsx
+++ packages/gocheok-project/src/ui/components/header/HeaderItem.tsx
@@ -1,6 +1,5 @@
 import { twMerge } from "tailwind-merge";
 import { TypographyProps } from "../typography/Typography";
-import "./header.css";
 
 export const HeaderItem = ({ children }: TypographyProps) => {
   return (
@@ -8,7 +7,9 @@ export const HeaderItem = ({ children }: TypographyProps) => {
       <a
         className={twMerge(
           "headerItem",
-          "relative text-(--color-neutral-100) text-md cursor-pointer transition duration-300 ease-in-out pb-2"
+          "relative text-(--color-neutral-100) text-md cursor-pointer transition duration-300 ease-in-out pb-2",
+          "after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-[0px] after:h-[2px] after:border-radius-1px after:opacity-0 after:bg-gradient-to-r after:from-[#ff6600] after:via-[#ff983f] after:to-[#ffffa1] after:transition-all after:duration-300 after:ease-in-out",
+          "hover:after:w-full hover:after:opacity-100"
         )}
       >
         {children}
