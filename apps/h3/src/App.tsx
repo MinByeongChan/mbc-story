8ed30a3657a59d02a8805ab53a03ae9d549d20cc
Min Byeong Chan <mbc0481@naver.com>
2026-02-06 08:10:24 +0900

    fix(h3): h3 to polygon 테스트

--- apps/h3/src/App.tsx
+++ apps/h3/src/App.tsx
@@ -5,6 +5,9 @@ import maplibregl from 'maplibre-gl';
 import { polygonToCells, compactCells } from 'h3-js';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import geojsonData from './assets/sig_4326.json';
+import { MainLayout } from '@/components/ui/MainLayout';
+import { Header } from '@/components/ui/Header';
+import { ContentLayout } from '@/components/ui/ContentLayout';
 
 const CENTER = { lat: 37.3595704, lng: 127.105399 };
 const DEFAULT_H3_RESOLUTION = 8;
@@ -24,43 +27,96 @@ type HoverInfo = {
 function App() {
   const mapContainerRef = useRef<HTMLDivElement | null>(null);
   const mapRef = useRef<maplibregl.Map | null>(null);
-  const resolutionOptions = [5,6,7,8,9,10,11,12];
+  const resolutionOptions = [5, 6, 7, 8, 9, 10, 11, 12];
   const [resolution, setResolution] = useState(DEFAULT_H3_RESOLUTION);
   const [hoverInfo, setHoverInfo] = useState<HoverInfo>(null);
 
-  const allH3Data = useMemo(() => {
+  const features = useMemo(() => {
     const data = geojsonData as unknown as GeoJSON.FeatureCollection;
-    const features = data.features.filter((feature) => feature.geometry.type === 'Polygon').slice(0, 30);
+    const features = data.features.filter((feature) => feature.geometry.type === 'Polygon');
+    console.log('features', features);
+    return features;
+  }, []);
 
-    return features.flatMap((feature) => {
-      const color = [ Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), 140];
-      const lineColor = [ Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), 200];
+  const getSigOptions = useMemo(() => {
+    return features.map((feature) => {
+      return {
+        id: feature.properties?.SIG_CD,
+        name: feature.properties?.SIG_ENG_NM,
+      };
+    });
+  }, [features]);
+  console.log('getSigOptions', getSigOptions);
+
+  const handleChangeSigSelectBox = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedSig = e.target.value;
+    console.log('selectedSig', selectedSig);
+    fetch(
+      `/req/address?service=address&request=getcoord&version=2.0&crs=EPSG:4326&format=json&type=PARCEL&key=${VWORLD_KEY}&address=서울특별시 강남구`,
+      // `/req/data?key=${VWORLD_KEY}&format=json&type=json&service=data&request=getfeature&featureType=attribute&featureKey=SIG_CD&featureValue=${selectedSig}`,
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('data', data);
+      })
+      .catch((error) => {
+        console.error('error', error);
+      });
+    console.log('selectedSig', selectedSig);
+  };
+
+  const allH3Data = useMemo(() => {
+    const slicedFeatures = features.slice(0, 30);
+
+    return slicedFeatures.flatMap((feature) => {
+      const color = [
+        Math.floor(Math.random() * 256),
+        Math.floor(Math.random() * 256),
+        Math.floor(Math.random() * 256),
+        140,
+      ];
+      const lineColor = [
+        Math.floor(Math.random() * 256),
+        Math.floor(Math.random() * 256),
+        Math.floor(Math.random() * 256),
+        200,
+      ];
       // @ts-ignore
       const cells = polygonToCells(feature.geometry.coordinates[0], resolution, true);
       const compacted = compactCells(cells);
-      
-      return compacted.map(h3Index => ({
+
+      return compacted.map((h3Index) => ({
         h3Index,
         color,
-        lineColor
+        lineColor,
       }));
     });
-  }, [resolution]);
+  }, [features, resolution]);
 
   const h3Layer = useMemo(() => {
     return new H3HexagonLayer({
       id: 'h3-layer-all',
-      data: allH3Data,      // 모든 H3 인덱스가 담긴 배열
-      getHexagon: (d) => d.h3Index,    // 배열 요소 자체가 H3 인덱스임
+      data: allH3Data, // 모든 H3 인덱스가 담긴 배열
+      getHexagon: (d) => d.h3Index, // 배열 요소 자체가 H3 인덱스임
       pickable: true,
       filled: true,
-      extruded: false,         // true면 3D로 돌출됨
+      extruded: false, // true면 3D로 돌출됨
       lineWidthMinPixels: 1,
       getFillColor: (d) => d.color,
       getLineColor: (d) => d.lineColor,
     });
   }, [allH3Data]);
-  
+
+  const handleMapMoveEnd = () => {
+    const currentCenter = mapRef.current?.getCenter();
+    console.log('현재 센터', currentCenter);
+  };
+
+  const handleMapZoom = () => {
+    const currentZoom = mapRef.current?.getZoom();
+    console.log('현재 줌 레벨:', currentZoom);
+  };
+
   useEffect(() => {
     if (!mapContainerRef.current || !VWORLD_KEY || mapRef.current) {
       return;
@@ -121,19 +177,8 @@ function App() {
       });
     });
 
-    map.on('mouseleave', () => {
-      const currentCenter = map.getCenter();
-      console.log('현재 센터', currentCenter);
-    });
-    
-    map.on('zoom', () => {
-      const currentZoom = map.getZoom();
-      // console.log('현재 줌 레벨:', currentZoom);
-      
-      // 예: 줌 레벨에 따라 H3 해상도를 동적으로 변경하려면?
-      // if (currentZoom > 15) setResolution(11);
-      // else if (currentZoom > 12) setResolution(9);
-    });
+    map.on('moveend', handleMapMoveEnd);
+    map.on('zoom', handleMapZoom);
 
     map.addControl(overlay);
     mapRef.current = map;
@@ -150,27 +195,42 @@ function App() {
   }
 
   return (
-    <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
-      <header style={{ padding: '12px 16px' }}>
-        <h3>VWorld + H3 WebGL 예제</h3>
+    <MainLayout>
+      <Header />
+      <ContentLayout>
+        <aside style={{ width: '250px', height: '100%', padding: '16px' }}>
+          <div>
+            <select onChange={handleChangeSigSelectBox}>
+              {getSigOptions.map((option) => (
+                <option key={option.id} value={option.name}>
+                  {option.name}
+                </option>
+              ))}
+            </select>
+          </div>
+        </aside>
         {/* <div>H3 셀 개수: {h3Data.length}</div> */}
-        <div>
-          <select value={resolution} onChange={(e) => setResolution(Number(e.target.value))}>
-            {resolutionOptions.map((option) => (
-              <option key={option} value={option}>{option}</option>
-            ))}
-          </select>
-        </div>
+        <section style={{ padding: '16px' }}>
+          <div>
+            <select value={resolution} onChange={(e) => setResolution(Number(e.target.value))}>
+              {resolutionOptions.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
 
-        <div style={{ height: '30px' }}>
-          info:
-          {hoverInfo
-            ? `${hoverInfo?.id} ${hoverInfo?.latitude.toFixed(5)} , ${hoverInfo?.longitude.toFixed(5)}`
-            : ''}
-        </div>
-      </header>
-      <div ref={mapContainerRef} style={{ width: '1000px', height: '500px' }} />
-    </div>
+          <div style={{ height: '30px' }}>
+            info:
+            {hoverInfo
+              ? `${hoverInfo?.id} ${hoverInfo?.latitude.toFixed(5)} , ${hoverInfo?.longitude.toFixed(5)}`
+              : ''}
+          </div>
+          <div ref={mapContainerRef} style={{ width: '1000px', height: '500px' }} />
+        </section>
+      </ContentLayout>
+    </MainLayout>
   );
 }
 
---
+++ apps/h3/src/components/ui/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header style={{ height: '60px', padding: '0 16px', borderBottom: '1px solid #e0e0e0' }}>
+      <h3>VWorld + H3 WebGL 예제</h3>
+    </header>
+  );
+};
